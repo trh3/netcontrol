e52c41ada7deb55f80424877572ba5a52eee0b15
Teague Henry <trhenry@live.unc.edu>
2019-09-23 15:45:47 -0400

    First Pass Documentation

--- R/control_stats.R
+++ R/control_stats.R
@@ -40,18 +40,64 @@ control_gramian <-function(A,B, t = NA){
 
 
 
+#' Trace of the Inverse Gramian
+#' 
+#' A commonly used measure of the overall controllability of a system defined by \eqn{x_(t+1) = Ax_(t) + Bu_(t)}.
+#'
+#' @param A An n by n matrix.
+#' @param B An n by m matrix.
+#'
+#' @return Trace of the inverse infinite time Gramian.
+#' @export
+#'
+#' @examples
+#' A = matrix(c(0,-3,-2,2,-2,1,-1,2,-1), 3,3)
+#' B = diag(3)
+#' 
+#' inv_average_control(A, B)
 inv_average_control <- function(A,B){
+  
   gramian <- control_gramian(A, B)
   return(sum(diag(pinv(gramian))))
   
 }
 
+#' Trace of the  Gramian
+#' 
+#' A commonly used measure of the overall controllability of a system defined by \eqn{x_(t+1) = Ax_(t) + Bu_(t)}.
+#'
+#' @param A An n by n matrix.
+#' @param B An n by m matrix.
+#'
+#' @return Trace of the infinite time Gramian.
+#' @export
+#'
+#' @examples
+#' A = matrix(c(0,-3,-2,2,-2,1,-1,2,-1), 3,3)
+#' B = diag(3)
+#' 
+#' average_control(A, B)
 average_control <- function(A, B){
   gramian <- control_gramian(A, B)
   return(sum(diag(gramian)))
   
 }
 
+#' Modal Control
+#' 
+#' Calculates the modal control of a system defined by \eqn{x_(t+1) = Ax_(t) + Bu_(t)}.
+#'
+#' @param A An n by n matrix.
+#' @param B An n by m matrix.
+#'
+#' @return An m by n matrix representing the control of the nth mode by the mth control input.
+#' @export
+#'
+#' @examples
+#' A = matrix(c(0,-3,-2,2,-2,1,-1,2,-1), 3,3)
+#' B = diag(3)
+#' 
+#' modal_control(A, B)
 modal_control <- function(A, B){
   
   
@@ -75,6 +121,19 @@ modal_control <- function(A, B){
   return(output)
 }
 
+#' Modal Control Centrality
+#' 
+#' Calculates the modal control centrality of a system defined by \eqn{x_(t+1) = Ax_(t)}.
+#'
+#' @param A An n by n matrix.
+#'
+#' @return A length n vector of modal control centrality measures, representing the overall modal control of each node in the system.
+#' @export
+#'
+#' @examples
+#' A = matrix(c(0,-3,-2,2,-2,1,-1,2,-1), 3,3)
+#' 
+#' modal_control_centrality(A, B)
 modal_control_centrality <- function(A){
   
   eig = eigen(A/(1+svd(A)$d[1]))
@@ -95,6 +154,19 @@ modal_control_centrality <- function(A){
   return(colSums(output))
 }
 
+#' Average Control Centrality
+#' 
+#' Calculates the modal control centrality of a system defined by \eqn{x_(t+1) = Ax_(t) + Bu_(t)}.
+#'
+#' @param A An n by n matrix.
+#'
+#' @return A length n vector of average control centrality measures, representing the overall average control of each node in the system.
+#' @export
+#'
+#' @examples
+#' A = matrix(c(0,-3,-2,2,-2,1,-1,2,-1), 3,3)
+#' 
+#' ave_control_centrality(A)
 ave_control_centrality <- function(A){
   
   centrality = diag(control_gramian(t(A)/(1+Mod(eigen(t(A))$values[1])), diag(dim(A)[[1]])))
